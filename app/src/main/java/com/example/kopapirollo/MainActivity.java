211ea74ceaab2e24f7810798641ef44cdf6f5e85
Zoltan Csoti <csoti.zoltan@gmail.com>
2020-10-19 08:11:44 +0200

    Kiértékelés (Evaluates metódus) működik

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -13,12 +13,14 @@ import android.widget.Toast;
 
 import java.util.Random;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imgGamer, imgComputer;
     private ImageView imgComputerHp1, imgComputerHp2, imgComputerHp3, imgGamerHp1, imgGamerHp2, imgGamerHp3;
     private TextView tvResult;
-    private Button btnKo, btnPapir, btnOllo;
+    private Button btnRock, btnPaper, btnScissors;
+    private AlertDialog.Builder AlertEndGame;
 
     private int hpGamer, hpComputer, hpEqual;
 
@@ -30,36 +32,37 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
-        btnKo.setOnClickListener(new View.OnClickListener() {
+        btnRock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 imgGamer.setImageResource(R.drawable.rock);
-                GepSorsol(0);
+                ComputerDraws(0);
             }
         });
 
-        btnPapir.setOnClickListener(new View.OnClickListener() {
+        btnPaper.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 imgGamer.setImageResource(R.drawable.paper);
-                GepSorsol(1);
+                ComputerDraws(1);
             }
         });
 
-        btnOllo.setOnClickListener(new View.OnClickListener() {
+        btnScissors.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 imgGamer.setImageResource(R.drawable.scissors);
-                GepSorsol(2);
+                ComputerDraws(2);
             }
         });
     }
 
-    private void GepSorsol(int gamer) {
+
+    private void ComputerDraws(int gamer) {
         int computer = (int)(Math.random() * 3);
 
         //Random rnd = new Random();
-        //int r = rnd.nextInt(3);
+        //int computer = rnd.nextInt(3);
 
 
         if (computer == 0) {
@@ -70,41 +73,89 @@ public class MainActivity extends AppCompatActivity {
             imgComputer.setImageResource(R.drawable.paper);
         }
 
-        else if (computer == 2) {
+        else {                  // computer == 2
             imgComputer.setImageResource(R.drawable.scissors);
         }
 
 
-        String eredmenyKiiras = "";
+        String resultMessage;
 
-        eredmenyKiiras = (gamer == 0 && computer == 2 || gamer == 1 && computer == 0 || gamer == 2 && computer == 1
+        resultMessage = (gamer == 0 && computer == 2 || gamer == 1 && computer == 0 || gamer == 2 && computer == 1
                 ? "Nyert" : computer == gamer ? "Döntetlen" : "Vesztet");
 
-        Toast.makeText(getApplicationContext(), eredmenyKiiras, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), resultMessage, Toast.LENGTH_SHORT).show();
 
-        Kiertekel(gamer, computer);
+        Evaluates(gamer, computer);
 
         tvResult.setText("Döntetlenek száma: " + hpEqual);
 
-        JatekVege();
-
-    }
-
-    private void Kiertekel(int gamer, int   computer) {
-
-        /*Szabályok
-        Kő veri az ollót.
-        Olló veri a papírt.
-        Papír veri a követ.*/
-
+        EngGame();
     }
 
 
-    private void JatekVege() {
+    private void Evaluates(int gamer, int   computer) {
+
+        if (gamer == 0 && computer == 2 || gamer == 1 && computer == 0 || gamer == 2 && computer == 1) {
+            hpGamer++;
+
+            if (hpGamer == 1) {
+                imgComputerHp1.setImageResource(R.drawable.heart1);
+            }
+
+            else if (hpGamer == 2) {
+                imgComputerHp2.setImageResource(R.drawable.heart1);
+            }
+
+            else {
+                imgComputerHp3.setImageResource(R.drawable.heart1);
+            }
+        }
+
+        else if (gamer == computer) {
+            hpEqual++;
+        }
+
+        else {
+            hpComputer++;
+
+            if (hpComputer == 1) {
+                imgGamerHp1.setImageResource(R.drawable.heart1);
+            }
+
+            else if (hpComputer == 2) {
+                imgGamerHp2.setImageResource(R.drawable.heart1);
+            }
+
+            else {
+                imgGamerHp3.setImageResource(R.drawable.heart1);
+            }
+        }
+    }
+
+
+    private void EngGame() {
 
         AlertDialog.Builder AlertEndGame = new AlertDialog.Builder(this);
 
+        if (hpGamer == 3 || hpComputer == 3) {
+            AlertEndGame = new AlertDialog.Builder(MainActivity.this);
+            AlertEndGame.setTitle("A játék véget ért.");
 
+            AlertEndGame.setCancelable(false).setMessage("Szeretne új játékot játszani?")
+                    .setPositiveButton("Nem", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            System.exit(0);
+                        }
+                    })
+                    .setNegativeButton("Igen", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            NewGame();
+                        }
+                    });
+            AlertEndGame.create().show();
+        }
     }
 
 
@@ -134,9 +185,9 @@ public class MainActivity extends AppCompatActivity {
         imgGamerHp2 = findViewById(R.id.imgGamerHp2);
         imgGamerHp3 = findViewById(R.id.imgGamerHp3);
         tvResult = findViewById(R.id.tvResult);
-        btnKo = findViewById(R.id.btnKo);
-        btnPapir = findViewById(R.id.btnPapir);
-        btnOllo = findViewById(R.id.btnOllo);
+        btnRock = findViewById(R.id.btnRock);
+        btnPaper = findViewById(R.id.btnPaper);
+        btnScissors = findViewById(R.id.btnScissors);
         hpGamer = 0;
         hpComputer = 0;
         hpEqual = 0;
